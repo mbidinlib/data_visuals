65a1d1f53af17ab4242efb8fe4adb0420e258820
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2022-09-17 17:48:17 -0500

    Initial commit

---
+++ data_visuals.py
@@ -0,0 +1,111 @@
+'''
+Date: Wed Aug 23 21:12:18 2022
+@author: Glooory
+Purpose: Data Engineering
+'''
+from io import StringIO
+import pandas as pd
+import streamlit as st
+
+# Set page outline and footer
+st. set_page_config(layout="wide")
+
+# Set the footer
+footer="""<style>
+a:link , a:visited{
+color: #D891FC;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: transparent;
+color: white;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a href="https://www.github.com/mbidinlib/" target="_blank">Mathew Bidinlib </a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
+
+
+
+
+# Define the sidebar
+st.sidebar.markdown("#Options")
+with st.sidebar.container():
+   sbcol1, sbcol2, = st.columns(2)
+
+   sbcol1.image('picture.jpg',
+        caption='Mathew Bidinlib', width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
+st.markdown("# MGlory Data Visualization")
+
+st.markdown("""
+    Welcome to MGlory Data Visualization toolkit.
+    This tool will help you visualize your data in charts, maps and others""")
+
+hcol1, hcol2 = st.columns(2)
+
+with hcol1:
+    st.markdown("<h4><u>Import Page</u></h4>",unsafe_allow_html=True)
+    exp1 = st.expander("Help note",expanded=False)
+    st.subheader("Select file")
+    ds = st.file_uploader("Select Data file", type=["csv", 'xlsx'], key = "data1")
+    st.markdown("")
+    st.markdown("")
+
+    # Read data
+    if "ds1" in st.session_state:
+        st.header("Data and Chart overview")  # Give it a header
+        df= st.session_state["ds1"]
+        file_ext = df.name.split('.')[-1]  # get file extension of selected file
+
+        if file_ext == 'csv':
+            dataset_1 = pd.read_csv(df)
+            st.dataframe(dataset_1) 
+            #st.dataframe(pd.read_csv(StringIO(df1),dtype='unicode')) ### Remove  
+            st.session_state["dataset1"] = dataset_1                         
+        elif file_ext == 'xls'or file_ext == 'xlsx':
+            dataset_1 = pd.read_excel(df1, engine='openpyxl').astype(str)
+            st.dataframe(dataset_1)
+            st.session_state["dataset1"] = dataset_1
+            #st.dataframe(pd.read_excel(df1))
+        else:
+            st.markdown("""**This file is type is currently not accepted. Upload a file with a .csv or xls extenssion. 
+            #Support for Other file extensions would be added later**""")
+
+
+
+with hcol1:
+    st.markdown("<h4><u>Outliers Page</u></h4>",unsafe_allow_html=True)
+    exp3 = st.expander("Help note",expanded=False)
+    exp3.write('''
+    This page is would help you check for outliers in your importd dataset
+    ''')
+
+
+
+
+with hcol2:
+    st.markdown("<h4><u>Duplicates Page</u></h4>",unsafe_allow_html=True)
+    exp2 = st.expander("Help note",expanded=False)
+    exp2.write('''
+    This page would help you check for duplicates in the imported datasets using some key variables. 
+    You would also be able to download duplicate reports and remove duplicates.
+    ''')
+
+
+
