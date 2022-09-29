f783791e01d835b6fbeec3c28ec31be736ebd6ed
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2022-09-29 00:05:48 +0000

    Update data_visuals.py

--- data_visuals.py
+++ data_visuals.py
@@ -8,6 +8,12 @@ import pandas as pd
 import streamlit as st
 import plotmaps as plotmap
 
+import folium
+import streamlit as st
+from folium.features import DivIcon
+from branca.element import Figure
+
+
 # Set page outline and footer
 st. set_page_config(layout="wide")
 
@@ -93,6 +99,98 @@ with hcol3:
         else:
             st.markdown("""**This file is type is currently not accepted. Upload a file with a .csv or xls extenssion. 
             #Support for Other file extensions would be added later**""")
+        
+        # Define plots
+        ##############
+        
+        
+        #Maps
+        # Step 1
+        loc = "Plot of Coordinates"
+        title_html = '''
+        <h1 style="text-align: center; font-size: 18px; font-weight: bold; color: 
+            #ffffff; background-color: #e02b20; padding: 10px 0px;margin:0px">{}</h1>
+        '''.format(loc)
+
+        data =  dataset
+
+        #Step 4
+        lat = data.lat
+        lon = data.lon
+        name = data.name 
+
+        fig2=Figure(width=550,height=350)
+        m = folium.Map(location=[(lat.max()+ lat.min())/2,
+                                (lon.max()+ lon.min())/2], zoom_start=5)
+
+
+        for i in range(0,len(data)):
+            folium.Marker(
+                location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
+                popup=data.iloc[i]['name'],
+                icon=folium.Icon(color = 'black', icon='info-sign')
+            ).add_to(m)
+
+
+        # Add custom base maps to folium
+        basemaps = {
+            'Google Maps': folium.TileLayer(
+                tiles = 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+                attr = 'Google',
+                name = 'Google Maps',
+                overlay = True,
+                control = True
+            ),
+            'Google Satellite': folium.TileLayer(
+                tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+                attr = 'Google',
+                name = 'Google Satellite',
+                overlay = True,
+                control = True
+            ),
+            'Google Terrain': folium.TileLayer(
+                tiles = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+                attr = 'Google',
+                name = 'Google Terrain',
+                overlay = False,
+                control = False
+            ),
+            'Google Satellite Hybrid': folium.TileLayer(
+                tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+                attr = 'Google',
+                name = 'Google Satellite',
+                overlay = True,
+                control = True
+            ),
+            'Esri Satellite': folium.TileLayer(
+                tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+                attr = 'Esri',
+                name = 'Esri Satellite',
+                overlay = True,
+                control = True
+            )
+        }
+
+
+
+        # Add custom basemaps
+        basemaps['Google Maps'].add_to(m)
+        basemaps['Google Satellite Hybrid'].add_to(m)
+
+        #Layers
+        fig2.add_child(m)
+        folium.LayerControl().add_to(m)
+
+        #Add title
+        m.get_root().html.add_child(folium.Element(title_html))
+        
+        #m.save('index.html')
+        #m.save('index.png') 
+
+            
+            
+            
+            
         if display == "Dataset":
             st.markdown("<h5><u>Data Overview</u></h5>",unsafe_allow_html=True)
             st.dataframe(dataset)
