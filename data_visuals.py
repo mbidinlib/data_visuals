8eb35cc51d4965f0501470ca6c8d7b0c55d80a70
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2022-09-29 22:42:06 +0000

    Update data_visuals.py

--- data_visuals.py
+++ data_visuals.py
@@ -111,6 +111,10 @@ with hcol1:
             yvar = st.selectbox(
                 'Select y variable',
                 columns)
+        # Add Color picker
+        if  xvar !="" and yvar !="":
+            scolor = st.color_picker('Plot color', '#0A8FBF')
+
         
         # Define geospatial inputs
         if type == 'Goespatial':
